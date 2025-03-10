24d2a8814e7fa91900d8f91dfc61db2a866e1f1e
diegomvaz <diegomartinsvaz@gmail.com>
2025-03-10 00:23:51 -0300

    Refactor About component description for clarity and conciseness

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -9,13 +9,14 @@ export default function About() {
             <div className='subheader content'>Sobre</div>
             <div className='line'></div>
             <div className='text content'>
-                Diego Vaz é um Analista de Dados com mais de 13 anos de experiência no mercado financeiro,
-                atualmente na Icatu Vanguarda, onde desenvolve soluções de dados, automatiza processos e
-                apoia decisões de investimento. Formado em Economia pela UERJ e em programação pela UFF,
-                com especializações em Ciência de Dados e Engenharia de Software, ele domina ferramentas
-                como Python, JavaScript, React.js, SQL e Power BI. Diego combina habilidades técnicas de
-                TI e de Mercado Financeiro para otimizar rotinas de back/middle office e implementar soluções
-                estratégicas de BI com foco em análise de dados e gestão de conhecimento.
+                Diego Vaz é um Analista de Dados com mais de 13 anos de experiência no mercado financeiro.
+                Atualmente, trabalha na Icatu Vanguarda, uma das maiores gestoras independentes de recursos do Brasil, onde desenvolve
+                soluções de dados e automações de processos visando gerar insights para os tomadores de decisão.
+                Formado em Economia pela UERJ e em programação pela UFF, com especializações em Ciência de Dados
+                e Engenharia de Software, ele domina ferramentas  como Python, JavaScript, React.js,
+                SQL e Power BI. Diego combina habilidades técnicas de  TI e de Mercado Financeiro para
+                otimizar rotinas de back/middle office e implementar soluções  estratégicas de BI com foco
+                em análise de dados e gestão de conhecimento.
             </div>
 
             <div className='info content'>
